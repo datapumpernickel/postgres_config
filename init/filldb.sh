de516be41555ef4511ea4602857f5c23222e38f8
paul bochtler <65470117+datapumpernickel@users.noreply.github.com>
2024-10-07 11:46:56 +0200

    remove env vars from local environment after use for making it safer

--- init/filldb.sh
+++ init/filldb.sh
@@ -18,6 +18,7 @@ readonly REQUIRED_ENV_VARS=(
 main() {
   check_env_vars_set
   init_user_and_db
+  unset db_user password database postgres_user
 }
 
 # Checks if all of the required environment
@@ -39,24 +40,24 @@ Aborting."
 
 # Performs the initialization in the already-started PostgreSQL
 # using the preconfigured POSTGRES_USER user.
-init_user_and_db() {
-  # local db_user=$(cat "$DB_USER")
-  # local password=$(cat "$DB_PASSWORD")
-  # local database=$(cat "$DB_DATABASE")
-  # local postgres_user=$(cat "$POSTGRES_USER_FILE")
 
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER_FILE" <<-EOSQL
-     CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';
-     CREATE DATABASE $DB_DATABASE;
-     GRANT ALL PRIVILEGES ON DATABASE $DB_DATABASE TO $DB_USER;
-     GRANT USAGE ON SCHEMA public TO $DB_USER;
-     GRANT CREATE ON SCHEMA public TO $DB_USER;
-     ALTER DATABASE $DB_DATABASE OWNER TO $DB_USER;
+init_user_and_db() {
+  local db_user=$(cat "$DB_USER")         # Read the actual content of the secret file
+  local password=$(cat "$DB_PASSWORD")    # Read the password from the file
+  local database=$(cat "$DB_DATABASE")    # Read the database name
+  local postgres_user=$(cat "$POSTGRES_USER_FILE")  # Read the PostgreSQL admin user
+
+  psql -v ON_ERROR_STOP=1 --username "$postgres_user" <<-EOSQL
+     CREATE USER $db_user WITH PASSWORD '$password';
+     CREATE DATABASE $database;
+     GRANT ALL PRIVILEGES ON DATABASE $database TO $db_user;
+     GRANT USAGE ON SCHEMA public TO $db_user;
+     GRANT CREATE ON SCHEMA public TO $db_user;
+     ALTER DATABASE $database OWNER TO $db_user;
 EOSQL
 }
-
-
 
 # Executes the main routine with environment variables
 # passed through the command line.
 main "$@"
+
